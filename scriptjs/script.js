1d167a7b6b26dfd40758f92116f464857ad9b0e3
Breno Farias <brenopalheta1999@gmail.com>
2022-02-07 11:26:19 -0300

    inserão de script

---
+++ scriptjs/script.js
@@ -0,0 +1,12 @@
+function visibilidade(idElem, idBtn){
+    let elementList = document.getElementById(idElem).style.display
+    
+    if(elementList == "block"){
+        document.getElementById(idElem).style.display = "none"
+        document.getElementById(idBtn).value = "Mostrar"
+    }
+    else {
+        document.getElementById(idElem).style.display = "block"
+        document.getElementById(idBtn).value =  "Esconder"
+    }
+}
