df3d13a68a4c1c6deb992fde468f8034e71b6394
Breno Farias <brenopalheta1999@gmail.com>
2022-02-18 11:06:03 -0300

    styles update

--- scriptjs/script.js
+++ scriptjs/script.js
@@ -1,9 +1,11 @@
 function visibilidade(idElem, idBtn){
     let elementList = document.getElementById(idElem).style.display
-    
+   
+
     if(elementList == "block"){
         document.getElementById(idElem).style.display = "none"
         document.getElementById(idBtn).value = "Mostrar"
+
     }
     else {
         document.getElementById(idElem).style.display = "block"
