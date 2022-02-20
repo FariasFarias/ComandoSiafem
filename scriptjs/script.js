9aae6099ea0acc435d60e170c77502f0d0698421
Breno Farias <brenopalheta1999@gmail.com>
2022-02-19 22:03:53 -0300

    Melhorias no script
    agora apenas um btn ficar ativo por vezes

--- scriptjs/script.js
+++ scriptjs/script.js
@@ -1,14 +1,28 @@
-function visibilidade(idElem, idBtn){
-    let elementList = document.getElementById(idElem).style.display
-   
 
-    if(elementList == "block"){
+//guarda elementos ativos no momento
+let btnAtivado = []
+
+function visibilidade(idElem, idBtn){
+    let elementIdStyle = document.getElementById(idElem).style.display
+    
+    if (btnAtivado.length != 0){
+        document.getElementById(btnAtivado[0]).style.display = "none"
+        document.getElementById(btnAtivado[1]).style.display = "Mostrar"
+        btnAtivado.splice(0,2)
+    }
+
+    if(elementIdStyle == "block"){
         document.getElementById(idElem).style.display = "none"
         document.getElementById(idBtn).value = "Mostrar"
+    } 
 
-    }
-    else {
+    //caso nenhum botão ativado 
+    else{
+
         document.getElementById(idElem).style.display = "block"
         document.getElementById(idBtn).value =  "Esconder"
-    }
+        
+        //listar iframe ativo
+        btnAtivado.push(idElem,idBtn)
+    } 
 }
